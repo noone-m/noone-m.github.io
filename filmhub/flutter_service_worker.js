fb8cfe359fa525a44f4c3491d5f95013d5062ac1
unknown <m1hdi1t@gmail.com>
2025-02-26 16:42:21 +0300

    fix bug

--- filmhub/flutter_service_worker.js
+++ filmhub/flutter_service_worker.js
@@ -3,9 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+const RESOURCES = {"assets/AssetManifest.bin": "b846841bc5e0e9021ecb133f16886bd1",
+"assets/AssetManifest.bin.json": "295d29710299d77164d4b27c8f21acfa",
+"assets/AssetManifest.json": "b4dafc2a3220302c894c0ad146397d38",
+"assets/assets/pubspec.lock": "f8961716b6ce9b9fc7173c83475c7932",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e88f100a25e100448d8f8a387dd03ffc",
 "assets/NOTICES": "a46e93d69b83250d6a24d547056ee90b",
@@ -23,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "4316c914d0347c0e1fe64db5de3b5608",
+"flutter_bootstrap.js": "730b08e5a1e9e4f2756ef2c05e248420",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "8e8a7c8fa64c456834c1b6da1da48ffe",
 "/": "8e8a7c8fa64c456834c1b6da1da48ffe",
-"main.dart.js": "8bf30d343e2e300714c712dba0d00dbf",
+"main.dart.js": "1dd5578d4937f71e8437b3b057c1bfd2",
 "manifest.json": "65a0e738bc1bf9faf78646ad560e5090",
 "version.json": "b4725be182283b92892941d9abf774da"};
 // The application shell files that are downloaded before a service worker can
